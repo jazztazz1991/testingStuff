57b8f54fdf45f3e4db3fafa466caaa64f639bb8e
Cody Chase <cchase1991@gmail.com>
2024-05-18 15:09:29 -0400

    finished python course

--- python/main.py
+++ python/main.py
@@ -5,10 +5,33 @@
 # print("he was " + character_age + " years old. ")
 
 # character_name = "Miles"
-# print("He really liked the name " + character_name + ", ")
+# print("He really liked the name " + character_name + ", 5")
 # print("but didn't like being " + character_age + ". ")
 
-phrase = "Giraffe Academy"
-num = 10
-phrase_len = len(phrase)
-print(phrase.replace("Giraffe", "Fluffy"))
+# phrase = "Giraffe Academy"
+# num = 10
+# phrase_len = len(phrase)
+# print(phrase.replace("Giraffe", "Fluffy"))
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + float(num2)
+# print(result)
+
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a Plural Noun: ")
+# celebrity = input("Enter a Celebrity: ")
+
+# print("Roses are " + color)
+# print(plural_noun + " are blue")
+# print("I love " + celebrity)
+
+#tuple - can not be changed once created
+# coordinates = (4,5)
+
+# print(coordinates[0])
+
+def say_hi():
+    print("Hi!")
+
+say_hi()
