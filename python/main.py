7ab718a0cca78d7cdce565448b27c3492527ae5f
Cody Chase <cchase1991@gmail.com>
2024-05-18 14:18:25 -0400

    learning python

---
+++ python/main.py
@@ -0,0 +1,14 @@
+# character_name = "Cody"
+# character_age = "35"
+
+# print("There once was a man named " + character_name + ",")
+# print("he was " + character_age + " years old. ")
+
+# character_name = "Miles"
+# print("He really liked the name " + character_name + ", ")
+# print("but didn't like being " + character_age + ". ")
+
+phrase = "Giraffe Academy"
+num = 10
+phrase_len = len(phrase)
+print(phrase.replace("Giraffe", "Fluffy"))
